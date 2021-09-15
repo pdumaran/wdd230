8a59caf8d6229d127c4393594c27b30dadcba0a2
pdumaran <pierrephild@gmail.com>
2021-09-16 03:36:54 +0800

    js update

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,4 @@ let myDate = new Date();
 let myYear = myDate.getFullYear();
 document.getElementById("myYear").textContent = myYear;
 document.getElementById("lu").textContent = "Last Updated: " +
-document.lastModified;
+    document.lastModified;

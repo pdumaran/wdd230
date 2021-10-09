c01ed578d5b46eb52a3a8237516ecb72531869fa
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-10 05:52:12 +0800

    added current date

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,6 +1,12 @@
 let myDate = new Date();
 let myYear = myDate.getFullYear();
+let month = myDate.getMonth() + 1;
+let day = myDate.getDate();
+let year = myDate.getFullYear();
 document.getElementById("myYear").textContent = myYear;
+document.getElementById("month").textContent = month;
+document.getElementById("day").textContent = day;
+document.getElementById("year").textContent = year;
 document.getElementById("lu").textContent = "Last Updated: " +
     document.lastModified;
 

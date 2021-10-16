135f07f394bafc6055152734b3566573374e26fe
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-16 22:54:23 +0800

    updates

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -13,3 +13,20 @@ document.getElementById("lu").textContent = "Last Updated: " +
 function toggleMenu() {    
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+/* JS-Message Based on Week Day */
+/* Watch Paul Cheney tutorial at https://video.byui.edu/media/JS-Message+Based+on+Week+Day/0_8im2g4rm */
+
+const today = new Date();
+console.log(today);
+
+    const dayNumber = today.getDay(); /* dayNumber means Monday is day 1, Tues is day 2, etc. */
+    console.log(dayNumber);
+
+    const element = document.getElementById("message");
+
+    if (dayNumber == 6) {
+        element.classList.add("showme");
+    } else {
+        element.classList.add("hideme");
+    }

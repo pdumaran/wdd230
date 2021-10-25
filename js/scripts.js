1754400b79fecc980b73baa52117d86177882daf
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-10-26 00:09:10 +0800

    update

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let myYear = myDate.getFullYear();
 document.getElementById("myYear").textContent = myYear;
 document.getElementById("lu").textContent = "Last Updated: " +
     document.lastModified;
+
+/* Call Webfont */
+WebFont.load({
+    google: {
+        families: [
+            'Roboto', 'Noto Sans JP', 'Open Sans'
+        ]
+    }
+});

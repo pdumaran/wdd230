1d37fcabf6d62b90dfa42da16b6b2eb5f4529b70
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-11-05 11:35:09 +0800

    Added table page

---
+++ lesson8/js/script.js
@@ -0,0 +1,30 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+let month = myDate.getMonth() + 1;
+let day = myDate.getDate();
+let year = myDate.getFullYear();
+document.getElementById("myYear").textContent = myYear;
+document.getElementById("month").textContent = month;
+document.getElementById("day").textContent = day;
+document.getElementById("year").textContent = year;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+/* JS-Message Based on Week Day */
+/* Watch Paul Cheney tutorial at https://video.byui.edu/media/JS-Message+Based+on+Week+Day/0_8im2g4rm */
+
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getDay(); /* dayNumber means Monday is day 1, Tues is day 2, etc. */
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+
+if (dayNumber == 5) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}

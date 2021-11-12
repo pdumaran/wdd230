a9efdffef4b33784fc21c088058048fefebb638f
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-11-12 15:39:33 +0800

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -8,10 +8,21 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
     prophets.forEach((prophet) => {
+      // Create the card and each elelement needed in the card
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
+      let birthdate = document.createElement('p');
+      let birthplace = document.createElement('p');
+      let image = document.createElement('img');
       h2.innerHTML = `${prophet.name} <span class="highlight">${prophet.lastname}</span>`;
+      
+      // Build the prophet card
       card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(birthplace);
+      card.appendChild(image);
+
+      // now output to the html document
       document.querySelector('div.cards').appendChild(card);
     });
   });

0a938fdb3649d3024b2729ce4c4897d50e070629
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-11-19 00:55:03 +0800

    update

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=1c916d2f38375f37807f064fcc377386`;
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        const currentTemp = document.querySelector('#current-temp');
+        currentTemp.textContent = jsObject.main.temp.toFixed(0);
+
+        const weatherIcon = document.querySelector('#icon');        
+        let imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        let imgalt = jsObject.weather[0].description;        
+        weatherIcon.setAttribute('src', imgsrc);
+        weatherIcon.setAttribute('alt', imgalt);
+    });

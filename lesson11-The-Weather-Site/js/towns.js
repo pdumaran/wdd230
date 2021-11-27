341bc67b39abf204fb8bec56111bf39a88626283
pdumaran <75081496+pdumaran@users.noreply.github.com>
2021-11-28 07:03:09 +0800

    update

---
+++ lesson11-The-Weather-Site/js/towns.js
@@ -0,0 +1,47 @@
+fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+  .then((response) => response.json())
+  .then((myInfo) => {
+    console.log(myInfo);
+    console.log(myInfo.towns[6]);
+	
+	for (let i = 0; i < myInfo.towns.length; i++) {
+            if (i == 2 || i == 6 || i == 3) {
+                let card = document.createElement('div');
+                
+                let info = document.createElement('section');
+                let photo = document.createElement('figure');
+                
+                let name = document.createElement('h2');
+                let motto = document.createElement('h3');
+                let date = document.createElement('p');
+                let people = document.createElement('p');
+                let rain = document.createElement('p');
+                let image = document.createElement('img');
+
+                //info.setAttribute('class', 'info');
+                
+                name.textContent = myInfo.towns[i].name;
+                motto.textContent = myInfo.towns[i].motto;
+                date.textContent = 'Year Founded: ' + myInfo.towns[i].yearFounded;
+                people.textContent = 'Population: ' + myInfo.towns[i].currentPopulation + 'souls';
+                rain.textContent = 'Annual Rain Fall: ' + myInfo.towns[i].averageRainfall + 'in';
+                image.setAttribute('src', 'images/'+myInfo.towns[i].photo);
+                image.setAttribute('alt', myInfo.towns[i].name + ' Hometown');
+                image.setAttribute('width', '100%');
+                image.setAttribute('height', '100%');
+                
+                info.appendChild(name);
+                info.appendChild(motto);
+                info.appendChild(date);
+                info.appendChild(people);
+                info.appendChild(rain);
+                card.appendChild(info);
+                
+                photo.appendChild(image);
+                card.appendChild(photo);
+
+                document.getElementById('display_here').appendChild(card);
+            }
+        }
+  }); //end of "then" fat arrow function
+
